2a9d9693001fc3de98689c9ab5a47b2d11b5c02c
Noa Kim <noanonoa@gmail.com>
2021-02-22 12:21:12 -0800

    calculator CSS complete

--- calculator/src/components/Calculator.jsx
+++ calculator/src/components/Calculator.jsx
@@ -1,29 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Calculator.css";
 
 function Calculator() {
+
+  const [display, setDisplay] = useState('0');
+
+  function inputDigit(digit) {
+    display === '0' ? setDisplay(String(digit)) : setDisplay(display + String(digit));
+  };
+
+  function inputDecimal() {
+    if (display.indexOf('.') === -1) setDisplay(display + '.');
+  };
+
+  function clearDisplay() {
+    setDisplay('0');
+  };
+
   return (
     <div className="calculator">
-      <div className="display">0</div>
+      <div className="display">{ display }</div>
       <div className="keypad">
-        <button className="btn function">AC</button>
+        <button className="btn function" onClick={() => clearDisplay()}>AC</button>
         <button className="btn function">+/-</button>
         <button className="btn function">%</button>
         <button className="btn operator">÷</button>
-        <button className="btn digit">7</button>
-        <button className="btn digit">8</button>
-        <button className="btn digit">9</button>
+        <button className="btn digit" onClick={() => inputDigit(7)}>7</button>
+        <button className="btn digit" onClick={() => inputDigit(8)}>8</button>
+        <button className="btn digit" onClick={() => inputDigit(9)}>9</button>
         <button className="btn operator">x</button>
-        <button className="btn digit">4</button>
-        <button className="btn digit">5</button>
-        <button className="btn digit">6</button>
-        <button className="btn operator">-</button>
-        <button className="btn digit">1</button>
-        <button className="btn digit">2</button>
-        <button className="btn digit">3</button>
+        <button className="btn digit" onClick={() => inputDigit(4)}>4</button>
+        <button className="btn digit" onClick={() => inputDigit(5)}>5</button>
+        <button className="btn digit" onClick={() => inputDigit(6)}>6</button>
+        <button className="btn operator minus">-</button>
+        <button className="btn digit" onClick={() => inputDigit(1)}>1</button>
+        <button className="btn digit" onClick={() => inputDigit(2)}>2</button>
+        <button className="btn digit" onClick={() => inputDigit(3)}>3</button>
         <button className="btn operator">+</button>
-        <button className="btn digit key-0">0</button>
-        <button className="btn digit">.</button>
+        <button className="btn digit key-0" onClick={() => inputDigit(0)}>0</button>
+        <button className="btn digit" onClick={() => inputDecimal()}>.</button>
         <button className="btn operator">=</button>
       </div>
     </div>
